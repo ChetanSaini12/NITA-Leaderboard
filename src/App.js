f4a8dcdf15e9d37123bb86ca48b080abda4e1d7c
chetansaini <chetansaini1241@gmail.com>
2023-02-16 02:02:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CfRating from "./Components/CfRating";
+function App() {
+  return (
+    <div className="App">
+      <CfRating/>
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/Components/CfRating.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Content from "./Content";
+import './CfRating.css'
+
+function CfRating() {
+  return (
+    <div>
+      <div className="mockup-window border bg-base-300">
+        <div className="flex justify-center px-4 py-6 xyz bg-base-200">
+            
+        <Content />
+
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default CfRating;

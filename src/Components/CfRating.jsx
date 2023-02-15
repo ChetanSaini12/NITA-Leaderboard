8534a4804a17352bd3978ea9e28b2978ae1cd5d5
chetansaini <chetansaini1241@gmail.com>
2023-02-16 03:56:17 +0530

    color

--- src/Components/CfRating.jsx
+++ src/Components/CfRating.jsx
@@ -5,9 +5,15 @@ import './CfRating.css'
 function CfRating() {
   return (
     <div>
-      <div className="mockup-window border bg-base-300">
+      <div className="mockup-window  border bg-base-300">
         <div className="flex justify-center px-4 py-6 xyz bg-base-200">
-            
+        {/* <div className="dropdown dropdown-hover">
+  <label tabIndex={0} className="btn m-1">Hover</label>
+  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</div> */}
         <Content />
 
         </div>
